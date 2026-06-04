2907837ddba7ff52f75c9c2ce7cbde87d9824d4b
singhlavanya809-byte <singhlavanya809@gmail.com>
2026-06-04 13:34:28 +0530

    Project -1
    to do list programme in python

---
+++ todoo.py
@@ -0,0 +1,25 @@
+tasks = []
+
+while True:
+    print("\n1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        task = input("Enter a task: ")
+        tasks.append(task)
+        print("Task added!")
+
+    elif choice == "2":
+        print("\nYour Tasks:")
+        for i, task in enumerate(tasks, start=1):
+            print(f"{i}. {task}")
+
+    elif choice == "3":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice!")
